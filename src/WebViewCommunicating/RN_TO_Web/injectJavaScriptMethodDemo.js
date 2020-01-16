fedfdce6db44c6bcdcece347674789cd7fa5470d
BYTTER\chenlw <chenlw@bytter.com>
2020-01-16 15:14:24 +0800

    优化

--- src/WebViewCommunicating/RN_TO_Web/injectJavaScriptMethodDemo.js
+++ src/WebViewCommunicating/RN_TO_Web/injectJavaScriptMethodDemo.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View,SafeAreaView} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 const run = `
